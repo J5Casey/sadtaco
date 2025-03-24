5e39aed240c871e7ac99a3b4e793a379e6f5aa41
J5Casey <jackhammerjack@hotmail.com>
2025-03-24 17:05:40 -0500

    Started adding SadTaco

---
+++ src/main/java/net/j5casey/sadtaco/item/custom/SadTacoItem.java
@@ -0,0 +1,9 @@
+package net.j5casey.sadtaco.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class SadTacoItem extends Item {
+    public SadTacoItem(Properties properties) {
+        super(properties);
+    }
+}
